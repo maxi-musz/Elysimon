4e4a853035978262bd0fc062d7dd90130e3aab2c
mayowa Bernard <bernardmayowa17@gmail.com>
2024-05-02 06:44:43 +0100

    Completed product listing

--- data/products.tsx
+++ data/products.tsx
@@ -39,7 +39,7 @@ export const products = [
         color: "Black",
         colorCode: "#000000",
         image:
-          "/images/",
+          "/images/camera.jpg",
       },
     ],
     reviews: [
@@ -56,7 +56,7 @@ export const products = [
           email: "example@gmail.com",
           emailVerified: null,
           image:
-            "https://lh3.googleusercontent.com/a/AAcHTteOiCtILLBWiAoolIW9PJH-r5825pBDl824_8LD=s96-c",
+            "/images/mouse.jpg",
           hashedPassword: null,
           createdAt: "2023-05-30T08:08:53.979Z",
           updatedAt: "2023-05-30T08:08:53.979Z",
@@ -79,19 +79,19 @@ export const products = [
         color: "Black",
         colorCode: "#000000",
         image:
-          "https://m.media-amazon.com/images/I/61g+McQpg7L._AC_SX679_.jpg",
+          "/images/phone.jpg",
       },
       {
         color: "Blue",
         colorCode: " #0000FF",
         image:
-          "https://m.media-amazon.com/images/I/713Om9vCHUL._AC_SX679_.jpg",
+          "/images/playstation.jpg",
       },
       {
         color: "Red",
         colorCode: "#FF0000",
         image:
-          "https://m.media-amazon.com/images/I/61thdjmfHcL.__AC_SX300_SY300_QL70_FMwebp_.jpg",
+          "/images/sample.jpg",
       },
     ],
     reviews: [
@@ -109,7 +109,7 @@ export const products = [
           email: "example1@gmail.com",
           emailVerified: null,
           image:
-            "https://lh3.googleusercontent.com/a/AAcHTtcuRLwWi1vPKaQOcJlUurlhRAIIq2LgYccE8p32=s96-c",
+            "/images/airpods.jpg",
           hashedPassword: null,
           createdAt: "2023-06-26T15:40:52.558Z",
           updatedAt: "2023-06-26T15:40:52.558Z",
@@ -129,7 +129,7 @@ export const products = [
           email: "example@gmail.com",
           emailVerified: null,
           image:
-            "https://lh3.googleusercontent.com/a/AAcHTteOiCtILLBWiAoolIW9PJH-r5825pBDl824_8LD=s96-c",
+            "/images/camera.jpg",
           hashedPassword: null,
           createdAt: "2023-05-30T08:08:53.979Z",
           updatedAt: "2023-05-30T08:08:53.979Z",
@@ -152,7 +152,7 @@ export const products = [
         color: "Graphite",
         colorCode: " #383838",
         image:
-          "https://m.media-amazon.com/images/I/61ni3t1ryQL.__AC_SX300_SY300_QL70_FMwebp_.jpg",
+          "/images/mouse.jpg",
       },
     ],
     reviews: [],
@@ -171,13 +171,13 @@ export const products = [
         color: "Black",
         colorCode: "#000000",
         image:
-          "https://m.media-amazon.com/images/I/71s4mjiit3L.__AC_SX300_SY300_QL70_FMwebp_.jpg",
+          "/images/airpods.jpg",
       },
       {
         color: "Silver",
         colorCode: "#C0C0C0",
         image:
-          "https://m.media-amazon.com/images/I/71zbWSRMaYL.__AC_SX300_SY300_QL70_FMwebp_.jpg",
+          "/images/alexa.jpg",
       },
     ],
     reviews: [],
